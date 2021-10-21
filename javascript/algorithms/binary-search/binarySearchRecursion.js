82bc1a20b112d8545afd8c9d2f7931b4d39e89f0
Sonny R. Recio <reciosonny@gmail.com>
2021-10-21 13:42:40 +0800

    Added binary search recursion implementation

---
+++ javascript/algorithms/binary-search/binarySearchRecursion.js
@@ -0,0 +1,40 @@
+function binarySearch(arr, startLeftIndex, startRightIndex, x) {
+
+  debugger;
+  console.log('trigger binary search');
+  if (startRightIndex >= startLeftIndex) {
+      let midIdx = startLeftIndex + Math.floor((startRightIndex - startLeftIndex) / 2);
+
+      // If the element is present at the middle
+      // itself
+      if (arr[midIdx] == x)
+          return arr[midIdx];
+
+      // If element is smaller than mid, then
+      // it can only be present in left subarray
+      if (arr[midIdx] > x)
+          return binarySearch(arr, startLeftIndex, midIdx - 1, x);
+
+      // Else the element can only be present
+      // in right subarray
+      return binarySearch(arr, midIdx + 1, startRightIndex, x);
+  }
+
+  // We reach here when element is not
+  // present in array
+  return -1;
+}
+
+let arr = [2, 3, 4, 10, 40];
+let x = 11;
+let n = arr.length
+let result = binarySearch(arr, 0, n - 1, x);
+(result == -1) ? console.log("Element is not present in array")
+                 : console.log("Element is present at: " +result);
+
+
+
+
+
+
+
